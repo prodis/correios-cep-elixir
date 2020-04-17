58dfcaeafd5a4a31f17f0500b391a4f87031a783
Prodis <prodis@gmail.com>
2020-04-18 00:07:08 +0200

    Skip Elixir format for fixture in Elixir 1.7.

--- .formatter.exs
+++ .formatter.exs
@@ -1,3 +1,14 @@
 [
-  inputs: ["{mix,.credo,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs:
+    if System.version() =~ ~r/1.7.*/ do
+      # Ignoring test/support because Elixir 1.7 formatter breaks on Travis.
+      [
+        "{mix,.credo,.formatter}.exs",
+        "{config,lib}/**/*.{ex,exs}",
+        "test/test_helper.exs",
+        "test/correios/**/*.{ex,exs}"
+      ]
+    else
+      ["{mix,.credo,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+    end
 ]
