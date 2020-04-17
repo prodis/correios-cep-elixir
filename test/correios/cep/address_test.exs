0a674626aa5c32a8e712489aaead0271b9fe4b88
Prodis <prodis@gmail.com>
2020-04-18 00:07:29 +0200

    Update Correios.CEP.Address.

--- test/correios/cep/address_test.exs
+++ test/correios/cep/address_test.exs
@@ -1,5 +1,51 @@
 defmodule Correios.CEP.AddressTest do
   use ExUnit.Case, async: true
 
-  doctest Correios.CEP.Address
+  alias Correios.CEP.Address, as: Subject
+
+  doctest Subject
+
+  describe "new/1" do
+    setup do
+      params = %{
+        street: 'Street',
+        neighborhood: 'Neighborhood',
+        complement: ' complement one',
+        complement2: nil,
+        city: 'City',
+        state: 'ST',
+        zipcode: '12345678'
+      }
+
+      {:ok, params: params}
+    end
+
+    test "creates a new address", %{params: params} do
+      expected_address = %Subject{
+        street: "Street",
+        neighborhood: "Neighborhood",
+        complement: "complement one",
+        city: "City",
+        state: "ST",
+        zipcode: "12345678"
+      }
+
+      assert Subject.new(params) == expected_address
+    end
+
+    test "when complement2 is given, joins it to the complement", %{params: params} do
+      params = %{params | complement2: "complement two"}
+
+      expected_address = %Subject{
+        street: "Street",
+        neighborhood: "Neighborhood",
+        complement: "complement one complement two",
+        city: "City",
+        state: "ST",
+        zipcode: "12345678"
+      }
+
+      assert Subject.new(params) == expected_address
+    end
+  end
 end
